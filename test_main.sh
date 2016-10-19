935a2d21e41340532ddb2a4daee732caf0d488d2
Oliver Cordes <ocordes@astro.uni-bonn.de>
2016-10-19 11:24:40 +0200

    - remove some old files - upgrade some README files

--- test_main.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#./acs-cte _drk /home/ocordes/svn/acs-cte/demo/jbke01rkq_drk.fits
-
-./acs-cte-opt demo/jbke01rkq_drkA.fits demo/testA.fits
--- test_main_opt.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./arctic -d 10 --out_float -c test_opt.cte $HOME/svn/arctic/demo/jbke01rkq_drkA.fits $HOME/svn/arctic/demo/testA.fits -m ACS $*
